36824118a93403e98fad9af770fb69cb1c7509bc
Artem Shishkin <a.shishkin02@gmail.com>
2024-12-13 01:29:08 +0300

    fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/test-task-mindbox',
   plugins: [react()],
   test: {
     globals: true,
